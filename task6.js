1803f986e7f30feb82c4f5574a39e5901ccf7d67
mjalala21 <jalalamusthari@gmail.com>
2026-07-17 12:26:16 +0530

    3658

--- task6.js
+++ task6.js
@@ -21,14 +21,56 @@
 
 
 
-var searchInsert = function(nums, target) {
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]>=target){
-        return i 
-        }
+// var searchInsert = function(nums, target) {
+//     for(let i=0;i<nums.length;i++){
+//         if(nums[i]>=target){
+//         return i 
+//         }
         
-   }
-   return nums.length
-    } 
+//    }
+//    return nums.length
+//     } 
 
-console.log(searchInsert([1,3,5,6,7,8,9,10],12))
+// console.log(searchInsert([1,3,5,6,7,8,9,10],12))
+
+// ------------------------------------------
+
+// 3658. GCD of Odd and Even Sums
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given an integer n. Your task is to compute the GCD (greatest common divisor) of two values:
+
+// sumOdd: the sum of the smallest n positive odd numbers.
+
+// sumEven: the sum of the smallest n positive even numbers.
+
+// Return the GCD of sumOdd and sumEven.
+
+ 
+
+// Example 1:
+
+// Input: n = 4
+
+// Output: 4
+
+// Explanation:
+
+// Sum of the first 4 odd numbers sumOdd = 1 + 3 + 5 + 7 = 16
+// Sum of the first 4 even numbers sumEven = 2 + 4 + 6 + 8 = 20
+// Hence, GCD(sumOdd, sumEven) = GCD(16, 20) = 4.
+
+// /**
+//  * @param {number} n
+//  * @return {number}
+//  */
+// var gcdOfOddEvenSums = function(n) {
+//     return n
+// };
+
+
+// -------------------------------------------------
