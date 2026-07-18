c8db270ccc428f32017d5e6959cb6794bae07cb7
mjalala21 <jalalamusthari@gmail.com>
2026-07-18 20:40:17 +0530

    2154

--- task6.js
+++ task6.js
@@ -110,3 +110,20 @@
 
   
 // };
+
+// _________________________________________________________________________
+// 2154. Keep Multiplying Found Values by Two
+// /**
+//  * @param {number[]} nums
+//  * @param {number} original
+//  * @return {number}
+//  */
+// var findFinalValue = function(nums, original) {
+   
+//    while(nums.includes(original)){
+//         original=original *2
+//        }
+    
+//     return original
+    
+// };
