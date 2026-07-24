1fec7e4f76afcd6c3a635299982b1d79a7daf3cf
mjalala21 <jalalamusthari@gmail.com>
2026-07-24 10:09:04 +0530

    2540

--- task6.js
+++ task6.js
@@ -206,3 +206,25 @@
 //     }
 //     return result
 // };
+// ___________________________________________
+
+// 2540. Minimum Common Value
+// var getCommon = function(nums1, nums2) {
+//     let i = 0;
+//     let j = 0;
+
+//     while (i < nums1.length && j < nums2.length) {
+
+//         if (nums1[i] === nums2[j]) {
+//             return nums1[i];
+//         }
+
+//         if (nums1[i] < nums2[j]) {
+//             i++;
+//         } else {
+//             j++;
+//         }
+//     }
+
+//     return -1;
+// };
